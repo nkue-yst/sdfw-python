6fe5644729b55105914204b31339bedc40a9a023
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-03-23 20:33:23 +0900

    First commit for Python sdfw

---
+++ sdfw/sdfw.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Nakaue Yoshito'
+__date__ = '2022/03/23'
+
+
+def sdfw_init():
+    """
+    Initialize engine components.
+    """
+    print('init')
+
+
+def sdfw_quit():
+    """
+    Quit process for engine components
+    """
+    print('quit')
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="sdfw",
+    version="0.1",
+    author="Nakaue Yoshito",
+)
